af16f3b78090c5466bf51fbfc22e4fa87c9c44b9
Greg Osagie <greg_osagie@yahoo.com>
2018-05-07 11:52:19 +0100

    Website Portfolio

---
+++ HTML/ text.js
@@ -0,0 +1,18 @@
+var text = "The quick fox jumped over the lazy dog.";
+var charCount = text.length;
+var currentLetterCount = 0;
+var speed = 100; // How fast should it type?
+var $input = $("some-textbox");
+
+function writeLetter() {
+    var currentText = $input.val();
+    var currentLetter = text.charAt(currentLetterCount);
+    currentLetterCount++;
+    $input.val(currentText + currentLetter);
+    if(currentLetterCount == charCount)
+        clearInterval(timerId);
+}
+
+var timerId = setInterval(writeLetter, speed);
+
+
